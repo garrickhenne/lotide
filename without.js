05a9d325191694d66465e43d03ddea91131c75d4
Garrick Henne <garrickhenne@outlook.com>
2023-09-19 14:15:22 -0700

    Check for array, add more tests

--- without.js
+++ without.js
@@ -25,6 +25,10 @@ const assertArraysEqual = function(arr1, arr2) {
 /* -------------------- Fake imports above :) --------------- */
 
 const without = function(originalArray, removalValues) {
+  if (!Array.isArray(removalValues) || !Array.isArray(originalArray)) {
+    return [];
+  }
+
   let filteredArray = [];
   for (const item of originalArray) {
     if (!removalValues.includes(item)) {
@@ -41,6 +45,12 @@ without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArraysEqual(without(['1', '2', '3'], [1, 2, '3']), ['1', '2']);
 assertArraysEqual(without([1, 1, 1, 1, 1, 2], [1, 3]), [2]);
+assertArraysEqual(without([2], []), [2]);
+assertArraysEqual(without([2], [3]), [2]);
+assertArraysEqual(without([2], 1), []);
+assertArraysEqual(without([2], undefined), []);
+assertArraysEqual(without([], [1]), []);
+assertArraysEqual(without([], []), []);
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
