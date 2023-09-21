86210b49e8e45ee4ce6dfbea798f850a3467eeee
Garrick Henne <garrickhenne@outlook.com>
2023-09-20 19:04:14 -0700

    Add map function

---
+++ map.js
@@ -0,0 +1,49 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    console.log('Both objects passed in must be arrays.');
+    return;
+  }
+
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: Array 1 is equal to Array 2!`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: Array 1 NOT EQUAL to Array 2`);
+    console.log(`Given: ${arr1} \n Expected: ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+// Our map function will take in two arguments:
+
+// An array to map
+// A callback function
+// The map function will return a new array based on the results of the callback function.
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+// TESTS
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+const results2 = map(words, word => word.length);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+
+const results3 = map(words, word => word.toUpperCase());
+assertArraysEqual(results3, ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
