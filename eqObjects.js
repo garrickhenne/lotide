ab32c2213a090456a56d87040cdff84b351c00e3
Garrick Henne <garrickhenne@outlook.com>
2023-09-21 18:37:32 -0700

    Allow nesting check for eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -25,8 +25,12 @@ const eqObjects = function(object1, object2) {
 
   for (const key in object1) {
     if (object1[key] !== object2[key]) {
+      // Value may be array so check eqArrays if so.
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         return eqArrays(object1[key], object2[key]);
+      } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object' && object1[key] !== null && object2[key] !== null) {
+        // Value may be another object so check recursively.
+        return eqObjects(object1[key], object2[key]);
       }
       return false;
     }
@@ -35,17 +39,37 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
+// TESTS STRETCH RECURSIVE
+const deeplyNestedExpected = {
+  a: {
+    b: {
+      c: {
+        d: 5
+      }
+    },
+    e: 3
+  }
+};
+const copy = JSON.parse(JSON.stringify(deeplyNestedExpected));
+assertEqual(eqObjects(deeplyNestedExpected, copy), true);
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+
+
+
 // TESTS
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
+// const shirtObject = { color: "red", size: "medium" };
+// const anotherShirtObject = { size: "medium", color: "red" };
+// assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
 
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
+// const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+// assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+// const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+// const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+// assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
 
-const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
+// const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+// assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
