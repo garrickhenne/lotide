3c2a099972a016ace268123deea0aa6d6015f736
Garrick Henne <garrickhenne@outlook.com>
2023-09-25 08:49:51 -0700

    Refactor eqArrays tests to file, export eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
 
@@ -22,19 +14,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-// TESTS
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // => should FAIL
-// assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), true); // => should FAIL
-// assertEqual(eqArrays([], []), true); // => should PASS
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,19 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+// TESTS
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false); // => should FAIL
+assertEqual(eqArrays([], []), true); // => should PASS
