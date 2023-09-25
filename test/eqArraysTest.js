9a03e78457e3ea103a07017459ea64cb3289dc90
Garrick Henne <garrickhenne@outlook.com>
2023-09-25 13:20:51 -0700

    Refactor eqArrays test file to mocha + chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,19 +1,36 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-// TESTS
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+describe('#eqArrays', () => {
+  it('returns true with nested arrays', () => {
+    assert.isTrue(eqArrays([[2, 3], [4]], [[2, 3], [4]]));
+  });
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+  it('returns false when nested arrays not equal', () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
+  });
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should FAIL
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false); // => should FAIL
-assertEqual(eqArrays([], []), true); // => should PASS
+  it('returns true for equal non-nested arrays', () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+
+  it('returns false for non-nested equal array but in different order arrays', () => {
+    assert.isFalse(eqArrays([1, 2, 3], [3, 2, 1]));
+  });
+
+  it('returns true for array of string values', () => {
+    assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+  });
+
+  it('does strict equality comparison', () => {
+    assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+  });
+
+  it('returns false for unequal lengthed arrays', () => {
+    assert.isFalse(eqArrays([1, 2], [1, 2, 3]));
+  });
+
+  it('returns true on two empty arrays', () => {
+    assert.isTrue(eqArrays([], []));
+  });
+});
