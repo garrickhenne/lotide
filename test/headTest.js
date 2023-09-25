4049584a8a40a47a02b566a47f1a2c5cbd0e4760
Garrick Henne <garrickhenne@outlook.com>
2023-09-25 09:13:12 -0700

    Add mocha and chai, refactor headTest to use testing framework

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,28 @@
-const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
-// TESTS
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head(1), undefined);
-assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
-assertEqual(head(undefined), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it('returns undefined, when given a non-array', () => {
+    assert.isUndefined(head(1));
+  });
+
+  it('returns undefined, when given an empty array', () => {
+    assert.isUndefined(head([]));
+  });
+
+  it('returns undefined, when given undefined', () => {
+    assert.isUndefined(undefined);
+  });
+});
